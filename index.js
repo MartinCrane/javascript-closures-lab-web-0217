caee4dfcff0f7e0ea879f035c3acc7abca80acb9
MartinCrane <martin.crane@gmail.com>
2017-03-17 14:10:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+
 const app = "I don't do much."
+
+function bumpCounter() {
+
+  var counter = 0;
+
+  function addBump() {
+    return counter = counter + 1;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+const createAnimal = (animalType) => {
+
+  const devious = (deadlyDevice) => {
+    return {
+      deadlyDevice,
+      animalType
+    }
+  }
+  return devious
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
